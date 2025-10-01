36ab9f6e2224070d5a05ed0dac7a17e95841bda0
Andrew Burger <andrewburger1991@gmail.com>
2025-10-01 15:49:16 +0200

    adding perform_dealing

--- substrate/client/dkg/src/lib.rs
+++ substrate/client/dkg/src/lib.rs
@@ -1,7 +1,22 @@
 pub mod communication;
 
 use std::{sync::Arc, marker::PhantomData};
+use rand::rngs::OsRng;
+use rand::RngCore;
 use sc_network::{NotificationService, ProtocolName};
+use adkg_vrf::dkg::transcript::{DkgTranscript};
+use adkg_vrf::dkg::{Ceremony, DkgResult};
+use adkg_vrf::dkg::dealer;
+
+use adkg_vrf::bls::threshold::ThresholdVk;
+use adkg_vrf::bls::vanilla::BlsSigner;
+
+use ark_ec::pairing::Pairing;
+use ark_ec::{CurveGroup, PrimeGroup};
+use ark_poly::GeneralEvaluationDomain;
+
+#[cfg(feature = "bls-experimental")]
+use sp_core::bls::bls381::*;
 
 const LOG_TARGET: &str = "dkg";
 
@@ -18,5 +33,26 @@ pub struct DkgNetworkParams<N, S> {
 	pub gossip_protocol_name: ProtocolName,
 
 	pub _phantom: PhantomData<(N, S)>,
+}
+
+pub fn perform_dealing<C: Pairing>() -> Option<DkgTranscript<C>> {
+	let mut os_rng = OsRng;
+
+	// TODO: Get this from somewhere
+	let num_validators = 42;
+	let f = num_validators;
+
+	let (n, t, k) = (3 * f + 1, 2 * f + 1, f + 1);
+	// TODO: Add real BLS keys.. These will be the bls keys for each validator
+    let signers: Vec<BlsSigner<C>> = (0..n)
+            .map(|_| BlsSigner::new(C::G2::generator(), &mut os_rng))
+            .collect();
+    let signers_pks: Vec<_> = signers.iter()
+            .map(|s| s.bls_pk_g2)
+            .collect();
+	let params = Ceremony::<C, GeneralEvaluationDomain<C::ScalarField>>::setup(t, &signers_pks);
+	let transcript = params.deal(&mut os_rng);
+
+	Some(transcript)
 }
 
